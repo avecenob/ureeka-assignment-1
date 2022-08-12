56fe2eb994097a17409aad19985570672d8fdc31
Arya Baskara Cahyakusuma <aryabasc@gmail.com>
2022-08-13 01:44:51 +0700

    Displayed all data items

--- pages/Home.js
+++ pages/Home.js
@@ -1,4 +1,4 @@
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 import React, { Component } from 'react'
 
 class Home extends Component {
@@ -31,14 +31,24 @@ class Home extends Component {
   render() {
     const { dataPlaces, isLoading } = this.state;
     return (
-      <View style={{flex: 1, padding: 24}}>
-      <Text>Hello</Text>
-        {isLoading ? <ActivityIndicator/> : (
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator /> : (
           <FlatList
             data={dataPlaces}
-            keyExtractor={({id}, index) => id}
-            renderItem={({item}) => (
-              <Text>{item['place-id']}. {item['place-name']}, {item['place-address']}</Text>
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <><Text>{item['place-id']}.{item['place-name']}</Text>
+                <Image
+                  style={{ height: 300 }}
+                  source={{ uri: item['place-thumb-image'] }}
+                />
+                <Text>Open time{'\n'}{item['open-time']}</Text>
+                <Text>Close time{'\n'}{item['close-time']}</Text>
+                <Text>Location{'\n'}{item['place-address']}</Text>
+                <Text>Min. budget{'\n'}Rp{item['place-budget-min']}</Text>
+                <Text>Max. budget{'\n'}Rp{item['place-budget-max']}</Text>
+                <Text>Rating{'\n'}{item['place-rating']}</Text>
+              </>
             )}
           />
         )}
